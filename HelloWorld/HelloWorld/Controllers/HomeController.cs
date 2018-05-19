76fb2ee6612a97b0016f7b65b6e1371858315fd1
vanchanhnguyen95 <vanchanhnguyen95@gmail.com>
2018-05-20 00:14:40 +0700

    Chanh Nguyen@DESKTOP-5FO4RF4 MINGW64 /d/GitData/LearningOnlineShopAsp (master)

---
+++ HelloWorld/HelloWorld/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using HelloWorld.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.WelcomeString = "Chào mừng bạn đến với ViewBag";
+
+            var message = new MessageModel();
+            message.Welcome = "Chào mừng đến với Models";
+            return View(message);
+        }
+    }
+}
---
+++ HelloWorld/HelloWorld/Models/MessageModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelloWorld.Models
+{
+    public class MessageModel
+    {
+        public string Welcome { get; set; }
+    }
+}
